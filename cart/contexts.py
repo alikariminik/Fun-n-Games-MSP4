f3c930c9deaa6742e4b5903056ec57d84d89afc4
alikariminik <alirkariminik@gmail.com>
2023-02-17 19:51:38 +0000

    updated context processor and logic for sale price to be added to subtotal in cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def cart_contents(request):
@@ -7,6 +9,27 @@ def cart_contents(request):
     cart_items = []
     sub_total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for product_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=product_id)
+        if product.get_price == product.price:
+            sub_total += quantity * int(product.price)
+            product_count += quantity
+            cart_items.append({
+                'product_id': product_id,
+                'quantity': quantity,
+                'product': product,
+            })
+        else:
+            sale_price = product.get_price()
+            sub_total += quantity * sale_price
+            product_count += quantity
+            cart_items.append({
+                'product_id': product_id,
+                'quantity': quantity,
+                'products': product,
+            })
 
     if sub_total < settings.FREE_DELIVERY:
         delivery = sub_total * Decimal(settings.STANDARD_DELIVERY / 100)
@@ -25,6 +48,7 @@ def cart_contents(request):
         'free_delivery_remainder': free_delivery_remainder,
         'free_delivery': settings.FREE_DELIVERY,
         'total': total,
+        'cart': cart,
     }
 
     return context
