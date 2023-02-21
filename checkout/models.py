8de234c133116a6cede0094ab9d661d6412d498d
alikariminik <alirkariminik@gmail.com>
2023-02-21 11:13:20 +0000

    added additional methods to checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,6 @@
+import uuid
+
+ 
 from django.db import models
 from django.conf import settings
 
@@ -23,6 +26,38 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_uniq_order_num(self):
+        """ Generate unique order number using UUID """
+
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY:
+            self.delivery_cost = self.order_total * \
+                settings.STANDARD_DELIVERY / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_uniq_order_num()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
@@ -34,3 +69,14 @@ class OrderLineItem(models.Model):
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2,
         null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        self. lineitem_total = self.product.get_price() * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.order.order_number}'
