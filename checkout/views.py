461fc2615cdb60a62afd5624ac3aad7da9a4b8d9
alikariminik <alirkariminik@gmail.com>
2023-02-21 12:21:41 +0000

    added forms, signals and admin

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, '')
+        return redirect(reverse('products'))
+
+    order_form = OrderForm
