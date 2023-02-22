189d530bc974ff30d60108650e9c73083f469ef3
alikariminik <alirkariminik@gmail.com>
2023-02-22 17:43:55 +0000

    added basic stripe functionality for checkout

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -1,6 +1,6 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
   base: {
@@ -35,4 +35,33 @@ card.addEventListener("change", function (event) {
   }
 });
 
+var form = document.getElementById('payment-form');
+
+form.addEventListener('btnSubmit ', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#btnSubmit ').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html).addClass("text-danger");
+            card.update({ 'disabled': false});
+            $('#btnSubmit').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+              console.log(result.paymentIntent.status)
+              form.submit();
+            }
+        }
+    });
+});
 
--- checkout/views.py
+++ checkout/views.py
@@ -6,25 +6,36 @@ from django.conf import settings
 from django.http import JsonResponse
 
 from .forms import OrderForm
-from . import urls
+from cart.contexts import cart_contents
 
 # Create your views here.
 
-stripe.api_key = settings.STRIPE_SECRET_KEY
-
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, '')
         return redirect(reverse('products'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MeEiBFpgDYd3hANnwqFJXH8RjxFLT4jYNz5iumZz5PDtjDPcvggBHsNshxXnIY7u68DnFyaVhJewXhvt0htFGfe00iOXtij2N',
-        'client_secret': 'sk_test_51MeEiBFpgDYd3hANLfclsr5NLyPrNHGHd5XeKdGZcKnnJHUIAjGA4HkiuIjt6zTo9iXbj2ZpLfjt70OKCMb0k3oN00tDojyrVa',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, template, context)
 
