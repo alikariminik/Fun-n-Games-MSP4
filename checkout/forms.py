6ff85cca221b6bb40b4c7ea33d79ee5af0af3ea8
alikariminik <alirkariminik@gmail.com>
2023-02-23 19:37:08 +0000

    added django.countries field in checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,20 +20,20 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postcode',
             'town_or_city': 'Town / City',
             'street_address1': 'Address Line 1',
             'street_address2': 'Address Line 2 (optional)',
-            'county': 'County (if applicable)',
+            'county': 'County, State or Locality (if applicable)',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from django.db.models import Sum
 from products.models import Product
 
+from django_countries.fields import CountryField
+
 
 class Order(models.Model):
     order_number = models.CharField(max_length=24, null=False, editable=False)
@@ -16,7 +18,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(
         max_digits=6, decimal_places=2, null=False, default=0)
