7d2cdf245577d50f98d4c2604c097377f697e8b4
alikariminik <alirkariminik@gmail.com>
2023-02-24 01:23:46 +0000

    added functionality to prefill checkout form from profile

--- checkout/views.py
+++ checkout/views.py
@@ -71,15 +71,43 @@ def checkout(request):
             messages.error(request, 'There was an error with the information\
                 you have provided. Please double check your information.')
 
-    current_cart = cart_contents(request)
-    total = current_cart['total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(request, f'Your cart is empty!')
+            return redirect(reverse(products))
 
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        current_cart = cart_contents(request)
+        total = current_cart['total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Missing: Stripe Public Key')
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
