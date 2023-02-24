c4eb3f8fd8bfa1582cdbbea1d16155f6dff34bfe
alikariminik <alirkariminik@gmail.com>
2023-02-24 00:37:37 +0000

    created profile pagge with saved delivery info and order history

--- checkout/views.py
+++ checkout/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render, redirect, \
 from django.contrib import messages
 from django.views.decorators.http import require_POST
 from django.conf import settings
-from products.models import Product
-from .models import OrderLineItem, Order
 
 from .forms import OrderForm
+from .models import OrderLineItem, Order
+
+from products.models import Product
+
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
+
 from cart.contexts import cart_contents
 
 import stripe
@@ -87,8 +92,30 @@ def checkout(request):
 
 
 def checkout_success(request, order_number):
+    """Handle successfully completed checkouts"""
+
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        order.user_profile = profile
+        order.save()
+
+        if save_info:
+            profile_data = {
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_town_or_city': order.town_or_city,
+                'default_county': order.county,
+                'default_postcode': order.postcode,
+                'default_country': order.country,
+                'default_phone_number': order.phone_number,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Your Order was Successful! \
         Your Order Number is {order_number}. \
         An email will sent to {order.email}  confirming this')
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
 from .models import UserProfile
+from .forms import UserProfileForm
+
+from checkout.models import UserProfile, Order
+
 
 # Create your views here.
 
@@ -8,9 +14,29 @@ def profile(request):
     """Display the User's profile"""
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)
+    orders = profile.Orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile
+        'form': form,
+        'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is the Order Confirmation for order number {order_number}.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
