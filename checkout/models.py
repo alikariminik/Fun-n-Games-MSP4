d95763d3e6f043052d6b4da065f7b4f2bbc13eeb
alikariminik <alirkariminik@gmail.com>
2023-02-25 16:09:46 +0000

    changed postcode field on order model to not required

--- checkout/models.py
+++ checkout/models.py
@@ -22,7 +22,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=80, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
     country = CountryField(blank_label='Country *', null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(
