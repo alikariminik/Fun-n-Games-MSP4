9db9b5536b758b82d4718dfc543675964ac40487
alikariminik <alirkariminik@gmail.com>
2023-02-25 09:56:42 +0000

    updated wsgi.py file

--- funngames/wsgi.py
+++ funngames/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fun-n-games.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'funngames.settings')
 
 application = get_wsgi_application()
